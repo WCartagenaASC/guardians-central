29da39fa994132ebf967a3fca5903af2ee77ee92
Guardians Central <167646142+GuardiansCentral@users.noreply.github.com>
2024-04-24 18:43:09 -0400

    import path module

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const getWeeklyRotatorsJS = require('./getWeeklyRotatorsJS');
 require('dotenv').config();
 const app = express();
